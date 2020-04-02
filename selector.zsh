154e99bf517c4613e0d84a862d60ba05c81e6fb6
Ivan Canet <ivan.canet@gmail.com>
2020-04-02 18:52:12 +0200

    feat(zsh): Added configuration for Zsh

---
+++ selector.zsh
@@ -0,0 +1,42 @@
+#!/usr/bin/env zsh
+
+[[ ! -v CLOVIS_CONFIG ]] && CLOVIS_CONFIG=~/config
+
+function source-selector-sh() {
+    emulate -L bash
+    builtin source "$@"
+}
+
+source-selector-sh "$CLOVIS_CONFIG"/selector.sh --no-completion --no-autofetch --no-autofetch-config "$@"
+
+# Completion
+autoload -Uz compinit
+compinit
+
+# Automatically CD to directories
+setopt autocd
+
+# Use Emacs key bindings
+bindkey -e
+
+# Options and traps defined in functions are local
+setopt localoptions localtraps
+
+# Commands in prompts should be expanded
+setopt promptsubst
+
+# History management
+HISTFILE=~/.zsh_history
+HISTSIZE=1000
+SAVEHIST=1000
+setopt appendhistory incappendhistory histignoredups
+
+# More powerful globs
+setopt extendedglob
+
+# Correct what I'm typing
+setopt correct
+
+# Get my prompt
+# shellcheck source=zsh-prompt.sh
+. "$CLOVIS_CONFIG"/zsh-prompt.sh
---
+++ zsh-prompt.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env zsh
+
+# Prompt
+autoload colors
+colors
+precmd() {
+	PS1=''
+
+	# Display username and hostname only when connected via SSH
+	if [[ -v SSH_CLIENT ]]; then
+		PS1+=' ‹%F{green}%n %F{cyan}%m%F{default}›'
+	fi
+
+	# Current directory
+	PS1+=' %F{blue}%~'
+
+	# Python virtual environment
+	if [[ -v VIRTUAL_ENV ]]; then
+		PS1+=" %F{default}⚕"
+	fi
+
+	# Last command's error status
+	PS1+='%(?..%F{red} %?)'
+
+	# Git status
+	SED_PATTERN='s/^/ %F{yellow}/;' # Yellow
+	SED_PATTERN+='s/[0-9]*↑/%F{green}&/;s/[0-9]*↓/%F{yellow}&/;s/⮁/%F{default}&/' # Ahead-after
+	PS1+="$(git data --status | sed "$SED_PATTERN")"
+
+	# Root or normal user?
+	PS1+=' %F{default}%(!.%F{red}⮕.→)'
+
+	# Reset color and final prompt
+	PS1+='%F{default} '
+}
